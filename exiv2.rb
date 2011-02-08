9a5f4a6dc7d90ef59f0ce9a37dd1b8badedaa790
Lucas Parry <lucas@envato.com>
2011-02-08 17:26:27 +1100

    herp derp derp

--- exiv2.rb
+++ exiv2.rb
@@ -1,4 +1,4 @@
 dep 'exiv2.src' do
   source "http://www.exiv2.org/exiv2-0.21.tar.gz"
-  met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.20")}/ }
+  met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.21")}/ }
 end
