0e7c6bae96555de27f52f14b7a46acd7c7503208
Ian Leitch <port001@gmail.com>
2011-03-09 14:57:08 +1100

    Regenerate ld.so.cache after installing exiv2

--- exiv2.rb
+++ exiv2.rb
@@ -1,4 +1,5 @@
 dep 'exiv2.src' do
   source "http://www.exiv2.org/exiv2-0.21.1.tar.gz"
   met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.21.1")}/ }
+  after { sudo "ldconfig" }
 end
