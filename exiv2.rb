0c0c3f907823097842bb6eb854e7e2432130f641
Ian Leitch <port001@gmail.com>
2011-03-09 14:49:17 +1100

    Bump exiv2 to 0.21.1

--- exiv2.rb
+++ exiv2.rb
@@ -1,4 +1,4 @@
 dep 'exiv2.src' do
-  source "http://www.exiv2.org/exiv2-0.21.tar.gz"
-  met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.21")}/ }
+  source "http://www.exiv2.org/exiv2-0.21.1.tar.gz"
+  met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.21.1")}/ }
 end
