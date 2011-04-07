25fd129ad2408889d7acaa6aba0e48cf6bf60882
Lucas Parry <lucas@envato.com>
2011-04-07 10:58:44 +1000

    where the hell does #helper come from?

--- cron.rb
+++ cron.rb
@@ -12,6 +12,7 @@
 meta :crontab do
   accepts_list_for :lines_to_add
   template {
+    raise self.inspect
     helper(:existing_crontab) { shell "crontab -l" }
     met? {
       existing_crontab && lines_to_add.all? { |lines| lines.all? { |schedule, command|
