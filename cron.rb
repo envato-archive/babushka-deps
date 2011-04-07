4b018c19fe22e2e27edf49374ace103a59479ce6
Lucas Parry <lucas@envato.com>
2011-04-07 11:39:56 +1000

    i think i just threw up in my mouth a little

--- cron.rb
+++ cron.rb
@@ -1,25 +1,41 @@
 # Add lines to your crontab!
 #
+# But not really! More like throw away your crontab and replace
+# it with this, unless all the commands exist in it. oh, and we
+# dont care about schedules. Sounds cleverer than it is.
+#
 # example:
 #
 # dep 'my crontab configured', :template => 'envato:crontab' do
-#   lines_to_add L{[
+#   env_vars_to_add lambda { ['MAILTO=bob@example.com', 'SOMETHING_ELSE=wow'] }
+#   lines_to_add lambda {[
 #     ["* * * * *", "#{var(:bin_path)}/ruby path/to/your/file.rb"],
 #     ["*/5 * * * *", "#{var(:bin_path)}/ruby path/to/another/file.rb"],
 #   ]}
 # end
 
 meta :crontab do
+  accepts_list_for :env_vars_to_add
   accepts_list_for :lines_to_add
+
+  def build_crontab
+    "#{env_vars_to_add.join("\n")}\n\n#{lines_to_add.map { |lines| lines.map { |schedule, command| "#{schedule}   #{command}" }.join("\n") }.join("\n")}\n"
+  end
+
   template {
     helper(:existing_crontab) { shell "crontab -l" }
+
     met? {
-      existing_crontab && lines_to_add.all? { |lines| lines.all? { |schedule, command|
-        existing_crontab[command]
-      }}
+      existing_crontab &&
+      lines_to_add.all? { |lines| lines.all? { |schedule, command|
+        existing_crontab.include? command
+      }} &&
+      env_vars_to_add.all? { |declaration|
+        existing_crontab.include? declaration
+      }
     }
     meet {
-      shell("crontab -", :input => lines_to_add.map { |lines| lines.map { |schedule, command| "#{schedule}   #{command}" }.join("\n") }.join("\n") + "\n")
+      shell("crontab -", :input => build_crontab)
     }
   }
 end
