9db884957a8e2b6016ab09fa1190eeacdb1930c1
Lucas Parry <lucas@envato.com>
2011-04-11 14:55:17 +1000

    exiv2 tango'd

--- exiv2.rb
+++ exiv2.rb
@@ -1,5 +1,15 @@
-dep 'exiv2.src' do
-  source "http://www.exiv2.org/exiv2-0.21.1.tar.gz"
-  met? { shell("exiv2 -V|head -n1") =~ /#{Regexp.escape("exiv2 0.21.1")}/ }
-  after { sudo "ldconfig" }
+class CronicInstaller < Tango::Runner
+  step "install" do
+    met? { shell("exiv2 -V|head -n1").output.include?("exiv2 0.21.1") }
+    meet {
+      source_url = 'http://www.exiv2.org/exiv2-0.21.1.tar.gz'
+      shell("rm", "-rf", "/tmp/exiv2")
+      shell("mkdir", "-p", "/tmp/exiv2")
+      shell("cd", "/tmp/exiv2")
+      shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
+      shell('tar', '-xvzf', File.basename(source_url))
+      shell('make') && shell('make install')
+      shell('ldconfig')
+    }
+  end
 end
