d75a4e8216dcb137d75c16542b5c77ce302626cf
Lucas Parry <lucas@envato.com>
2011-04-11 15:50:32 +1000

    refactoring out build-from-source

--- exiv2.rb
+++ exiv2.rb
@@ -11,29 +11,36 @@ class AptInstaller < Tango::Runner
   end
 end
 
-class Exiv2Installer < Tango::Runner
+class SourceInstaller < Tango::Runner
   def initialize
     @apt = AptInstaller.new
   end
 
-  step "install" do #requires root
-
-    met? { shell("exiv2 -V|head -n1").output.include?("exiv2 0.21.1") }
-    meet do
-      @apt.install('build-essential')
-      @apt.install('zlib1g-dev')
-      @apt.install('libexpat1-dev')
-      source_url = 'http://www.exiv2.org/exiv2-0.21.1.tar.gz'
-      shell("rm", "-rf", "/tmp/exiv2")
-      shell("mkdir -p /tmp/exiv2")
-      cd "/tmp/exiv2" do
-        shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
-        shell('tar', '-xvzf', File.basename(source_url))
-        cd File.basename(source_url, '.tar.gz') do
+  step 'install' do |source_url|
+    @apt.install('build-essential')
+    cd '/tmp' do
+      shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
+      shell('tar', '-xvzf', File.basename(source_url))
+      cd File.basename(source_url, '.tar.gz') do
         shell('./configure').succeeded? && shell('make').succeeded? && shell('make install')
         shell('ldconfig')
-        end
       end
+    end
+  end
+end
+
+class Exiv2Installer < Tango::Runner
+  def initialize
+    @apt = AptInstaller.new
+    @src_installer = SourceInstaller.new
+  end
+
+  step "install" do #requires root
+    met? { shell("exiv2 -V|head -n1").output.include?("exiv2 0.21.1") }
+    meet do
+      @apt.install('zlib1g-dev')
+      @apt.install('libexpat1-dev')
+      @src_installer.install('http://www.exiv2.org/exiv2-0.21.1.tar.gz')
     end
   end
 end
