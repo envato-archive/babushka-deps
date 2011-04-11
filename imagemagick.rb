cfc24b90236f934c36c6561f9808477f8b1a5903
Lucas Parry <lucas@envato.com>
2011-04-11 16:37:31 +1000

    imagemagick dep

--- imagemagick.rb
+++ imagemagick.rb
@@ -1,12 +1,23 @@
-dep 'imagemagick.src' do
-  requires %w(gcc perl pkg-config).map {|i| dep "#{i}.managed" }
-  # libz-dev libltdl3-dev
-  requires(%w[graphviz libperl-dev gs-gpl libjpeg62-dev libbz2-dev libtiff4-dev libwmf-dev libpng12-dev libx11-dev libxt-dev libxext-dev libxml2-dev libfreetype6-dev liblcms1-dev libexif-dev libjasper-dev].map do |i|
-    dep "#{i}.managed" do
-      provides []
+require 'apt_installer'
+require 'source_installer'
+class ImageMagickInstaller < Tango::Runner
+  def initialize
+    @apt = AptInstaller.new
+    @src_installer = SourceInstaller.new
+  end
+
+  step "install" do
+    #%w(gcc perl pkg-config).map {|i| dep "#{i}.managed" }
+    # libz-dev libltdl3-dev
+
+    met? { File.exist?('/usr/local/bin/convert') }
+
+    meet do
+      %w[graphviz libperl-dev gs-gpl libjpeg62-dev libbz2-dev libtiff4-dev libwmf-dev libpng12-dev libx11-dev libxt-dev libxext-dev libxml2-dev libfreetype6-dev liblcms1-dev libexif-dev libjasper-dev].each do |pkg|
+        @apt.install pkg
+      end
+
+      @src_installer.install "http://downloads.sourceforge.net/project/imagemagick/old-sources/6.x/6.4/ImageMagick-6.4.9-10.tar.gz"
     end
-  end)
-  source "http://downloads.sourceforge.net/project/imagemagick/old-sources/6.x/6.4/ImageMagick-6.4.9-10.tar.gz"
-  provides 'convert'
-  after { sudo "ldconfig" }
+  end
 end
