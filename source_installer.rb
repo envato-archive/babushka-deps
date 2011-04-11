c68bdce8bb1571714ef9379f239ed078736bf420
Lucas Parry <lucas@envato.com>
2011-04-11 16:37:11 +1000

    source installer dep

---
+++ source_installer.rb
@@ -0,0 +1,17 @@
+class SourceInstaller < Tango::Runner
+  def initialize
+    @apt = AptInstaller.new
+  end
+
+  step 'install' do |source_url|
+    @apt.install('build-essential')
+    cd '/tmp' do
+      shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
+      shell('tar', '-xvzf', File.basename(source_url))
+      cd File.basename(source_url, '.tar.gz') do
+        shell('./configure').succeeded? && shell('make').succeeded? && shell('make install')
+        shell('ldconfig')
+      end
+    end
+  end
+end
