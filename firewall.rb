ae92db794a030edd5d53880251095a7e606b0253
Lucas Parry <lucas@envato.com>
2011-04-11 14:17:06 +1000

    firewall deps

--- firewall.rb
+++ firewall.rb
@@ -1,20 +1,13 @@
-dep 'firewall enabled' do
-  requires_when_unmet 'firewall permits SSH'
-  met? { sudo("ufw status")["Status: active"] }
-  meet { sudo "ufw -f enable" }
-end
+class FirewallEnabler < Tango::Runner
+  step "enable" do
+    shell("ufw allow ssh") #always make sure we're not locking ourselves out
+    met? { shell("ufw status").output.include?("Status: active") }
+    meet { sudo("ufw -f enable") }
+  end
 
-dep 'firewall disabled' do
-  requires_when_unmet 'firewall permits SSH'
-  met? { sudo("ufw status")["Status: inactive"] }
-  meet { sudo "ufw -f disable" }
-end
-
-dep 'firewall permits SSH' do
-  # Sadly, can't figure out a way to test if UFW is permitting SSH without turning it on
-  met? { @firewall_permits_ssh_run }
-  meet {
-    sudo "ufw allow ssh"
-    @firewall_permits_ssh_run = true
-  }
+  step "disable" do
+    shell("ufw allow ssh") #always make sure we're not locking ourselves out
+    met? { shell("ufw status").output.include?("Status: inactive") }
+    meet { shell("ufw -f disable") }
+  end
 end
