7473686658edcf93666ca722df81aa9416fee17d
Lucas Parry <lucas@envato.com>
2011-04-12 12:32:13 +1000

    db converted

--- db.rb
+++ db.rb
@@ -1,26 +1,38 @@
-# A cut-down of benhoskings db setup deps. This is pretty specific to our
-# needs: mysql, root user, no password.
+require 'apt_installer'
+require 'gem_installer'
+require 'yaml'
 
-dep 'mysql db created' do #DONE
-  requires 'mysql.managed' #DONE
-  setup {
-    if (db_config = yaml(var(:rails_root) / 'config/database.yml')[var(:rails_env)]).nil?
-      log_error "There's no database.yml entry for the #{var(:rails_env)} environment."
+class MysqlInstaller < Tango::Runner
+  def initialize
+    @apt = AptInstaller.new
+    @gem = GemInstaller.new
+  end
+
+  step 'mysql_db_created' do
+    @rails_root = '/data/marketplace/current/'
+    @rails_env = 'production'
+    install_mysql
+    db_config = YAML.load_file(File.join(@rails_root,'config/database.yml'))[@rails_env]
+
+    if db_config.nil?
+      raise "There's no database.yml entry for the #{@rails_env} environment."
     else
-      set :db_name, db_config['database']
+      @db_name = db_config['database']
     end
-  }
-  # figure out root passwords / users / etc later.
-  met? { shell("echo 'SHOW DATABASES;' | mysql -u root").split("\n")[1..-1].any? {|l| /\b#{var :db_name}\b/ =~ l } }
-  meet { shell "echo 'CREATE DATABASE #{var :db_name};' | mysql -u root" }
-end
 
-dep 'mysql.managed' do
-  installs { via :apt, %w[mysql-server libmysqlclient16-dev] }
-  provides 'mysql'
-end
+    raise "No database name was configured in database.yml" unless @db_name
 
-dep 'mysql2.gem' do
-  requires 'mysql.managed'
-  provides []
+    met? { shell("echo 'SHOW DATABASES;' | mysql -u root").output.split("\n")[1..-1].any? {|line| /\b#{@db_name}\b/ =~ line } }
+    meet { shell("echo 'CREATE DATABASE #{@db_name};' | mysql -u root") }
+  end
+
+  step 'install_mysql' do
+    %w[mysql-server libmysqlclient16-dev].each{|p| @apt.install(p) }
+  end
+
+  step 'install_mysql2_gem' do
+    install_mysql
+    @apt.install('ruby1.8-dev')
+    @gem.install('mysql2')
+  end
 end
