5ac8fd4237b10d9b50e1cd702836fe6ac7eaa843
Lucas Parry <lucas@envato.com>
2011-04-12 10:14:38 +1000

    fixing bug in the source installer

--- source_installer.rb
+++ source_installer.rb
@@ -1,3 +1,4 @@
+require 'fileutils'
 class SourceInstaller < Tango::Runner
   def initialize
     @apt = AptInstaller.new
@@ -6,6 +7,7 @@ class SourceInstaller < Tango::Runner
   step 'install' do |source_url|
     @apt.install('build-essential')
     cd '/tmp' do
+      FileUtils.rm_f(File.join('/tmp/', File.basename(source_url)))
       shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
       shell('tar', '-xvzf', File.basename(source_url))
       cd File.basename(source_url, '.tar.gz') do
