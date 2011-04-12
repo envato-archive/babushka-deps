a45d725fe09708026c16003ebf6bb43661da234c
Pete Yandell <pete@notahat.com>
2011-04-12 11:03:13 +1000

    Stripped down the ImageMagick dependencies.

--- imagemagick.rb
+++ imagemagick.rb
@@ -1,23 +1,20 @@
 require 'apt_installer'
 require 'source_installer'
+
 class ImageMagickInstaller < Tango::Runner
   def initialize
-    @apt = AptInstaller.new
-    @src_installer = SourceInstaller.new
+    @apt    = AptInstaller.new
+    @source = SourceInstaller.new
   end
 
   step "install" do
-    #%w(gcc perl pkg-config).map {|i| dep "#{i}.managed" }
-    # libz-dev libltdl3-dev
-
-    met? { File.exist?('/usr/local/bin/convert') }
+    met? { shell("convert --version").succeeded? }
 
     meet do
-      %w[graphviz libperl-dev gs-gpl libjpeg62-dev libbz2-dev libtiff4-dev libwmf-dev libpng12-dev libx11-dev libxt-dev libxext-dev libxml2-dev libfreetype6-dev liblcms1-dev libexif-dev libjasper-dev].each do |pkg|
-        @apt.install pkg
-      end
-
-      @src_installer.install "http://downloads.sourceforge.net/project/imagemagick/old-sources/6.x/6.4/ImageMagick-6.4.9-10.tar.gz"
+      @apt.install "libjpeg62-dev"
+      @apt.install "libpng12-dev"
+      url = "http://downloads.sourceforge.net/project/imagemagick/old-sources/6.x/6.4/ImageMagick-6.4.9-10.tar.gz"
+      @source.install(url, :configure_options => %w{--with-jpeg=yes --with-png=yes --with-perl=no})
     end
   end
 end
--- source_installer.rb
+++ source_installer.rb
@@ -4,14 +4,17 @@ class SourceInstaller < Tango::Runner
     @apt = AptInstaller.new
   end
 
-  step 'install' do |source_url|
+  step 'install' do |*args|
+    source_url, options = *args
+    options ||= {}
+
     @apt.install('build-essential')
     cd '/tmp' do
       FileUtils.rm_f(File.join('/tmp/', File.basename(source_url)))
       shell('wget', source_url).succeeded? or raise("curl failed to download #{source_url}")
       shell('tar', '-xvzf', File.basename(source_url))
       cd File.basename(source_url, '.tar.gz') do
-        shell('./configure').succeeded? && shell('make').succeeded? && shell('make install')
+        shell('./configure', *options[:configure_options]).succeeded? && shell('make').succeeded? && shell('make install')
         shell('ldconfig')
       end
     end
